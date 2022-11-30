273293248e16c03b494f8a94f794296d51d3628a
ayaasayeedd <ayaasayeedd@gmail.com>
2022-11-30 21:24:33 +0200

    first commit

---
+++ lab1.js
@@ -0,0 +1,57 @@
+
+//-----------------------(1)-------------
+
+// var temp = prompt("Enter the Temp. degree:");
+// (temp >=30 ? console.log("HOT"): console.log("COLD") )
+
+//-------------------------------(2)---------------------------
+
+// var name=prompt("Enter your name:")
+// while(isFinite(name)){
+//   var name=prompt("Enter your name:")
+// }
+// console.log("Name: ",name)
+
+// var bd=prompt("Enter your birthday date that's lesss than 2010:")
+// while(isNaN(bd) || bd > 2010){
+//     var bd=prompt("Enter your birthday date that's lesss than 2010:")
+// }
+// console.log("Birthday:",bd)
+// console.log("Age:",2022-bd)
+
+//----------------------------------------------(3)------------------------------- 
+
+//  var a=parseInt(prompt("Enter the coaffecient A"));
+//  var b=parseInt(prompt("Enter the coaffecient B"));
+//  var c=parseInt(prompt("Enter the coaffecient C"));
+
+//  var brac= (b*b)-(4*a*c);
+
+//  var r1 = ((-b + Math.sqrt(brac)) / (2*a))
+//  var r2 = ((-b - Math.sqrt(brac)) / (2*a))
+//   console.log("first= ",r1)
+//  console.log("second= ",r2)
+
+
+//--------------------------------(4)----------------------------
+
+ /* var m=prompt("enter the first digit")
+ var n=prompt("enter the second digit")
+ var sum= (m%10) + (n%10);
+
+console.log("the sum = ",sum) ; */
+
+//------------------------------(5)-----------------
+
+
+// var d1=prompt("Enter the first distance:")
+// console.log("input:",d1)
+// var d2= d1*2;
+// console.log("output:",d2)
+
+//---------------------------//-------------------------------
+
+
+
+
+
