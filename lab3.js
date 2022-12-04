304fd25dadca31a1b208ef312e5cbba49923d85f
Aya Sayed Abdelghany <112312552+ayaasayeedd@users.noreply.github.com>
2022-12-04 22:49:20 +0200

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,95 @@
+ 
+//----------------------(1)----------------------------------------
+
+// var window1;
+//  function f(){
+//    window1= window.open('./child.html',"","_blank");
+//  }
+
+//  function c(){
+//    window1.close();
+// }
+
+//----------------------------(2)---------------------------
+
+
+// function startclock(){
+
+//   alert("Clock Started");
+//   t();
+// }
+// function t(){
+//    const ti= new Date();
+//    var h= ti.getHours();
+//    var m= ti.getMinutes();
+//    var s=ti.getSeconds();
+//    // document.write(h,":",m,":",s);
+//    document.getElementById("time").innerHTML=h+":"+m+":"+s;
+//    // x.innethtml=time.tolocalTimeString();
+//    setTimeout(t,1000);
+
+// }
+
+//---------------------------(3)-----------------------------
+
+// var window1;
+//  function o(){
+//    window1= window.open('./child.html',"","_blank");
+   
+//  }
+
+//  function go(){
+//    setTimeout(o,3000)
+// }
+
+// var text="aya sayed";
+// var counter;
+// var i=0;
+
+// function paragraph(){
+
+// document.write(text[i])
+
+//   i++
+
+//  if(i >= text.length){
+//  clearInterval(counter)
+// }
+
+// }
+
+//  counter=setInterval(paragraph,1000);
+
+//----------------------------(4)--------------------------------
+
+// function scrolling()
+// {
+//     if(scrollY>777)
+//     {
+//         func();
+//     }
+// }
+
+// function func() {
+//     alert("HI :)");
+//   }
+
+  //--------------------------------------(5)-------------------------------------
+
+// var mywind;
+
+// mywind = window.open("./child.html","","_blank")
+// mywind.document.write("typing message...");
+
+// function closewind(){
+//     mywind.close();
+// }
+
+// setTimeout(closewind,3000)
+
+//--------------------------------(6)----------------------------------------------
+// function func(){
+//  var mywind=window.open("./child.html","","width=700,height=700")
+//   mywind=window.scrollTo(0,700)
+// }
+// func()
